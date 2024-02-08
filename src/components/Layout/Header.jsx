4a67151704f22f72a147b3753267baf78cb7d003
pricnenishad904 <princenishad904@gmail.com>
2024-02-08 14:15:34 +0530

    Add logo

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -3,6 +3,7 @@ import prince from "../../assets/prince.jpg";
 import ProfileCard from "../ProfileCard";
 import SearchBar from "../SearchBar";
 import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo.jpg";
 
 const Header = () => {
   const [showProfile, setShowProfile] = useState(false);
@@ -11,7 +12,9 @@ const Header = () => {
     <nav className="sticky top-0 bg-white dark:bg-slate-800 max-md:pb-2 shadow-md">
       <div className="w-full h-14  flex justify-between items-center px-8 dark:bg-slate-800 dark:text-white ">
         <NavLink to={"/"}>
-          <button>Digital Notes</button>
+          <div className="w-44 max-md:w-32 max-sm:w-28">
+            <img src={logo} className="rounded-md" alt="" />
+          </div>
         </NavLink>
         <SearchBar hideSearchBar={"max-md:hidden"} searchWidth={"w-6/12"} />
         <button
